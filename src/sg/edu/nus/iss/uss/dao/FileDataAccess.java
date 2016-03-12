fb2561f7b2b490bc81fbc279c98b6d54e5a8ec58
benjaminng <benjaminng@DESKTOP-DL9RA9V>
2016-03-12 15:26:32 +0800

    add validation

--- src/sg/edu/nus/iss/uss/dao/FileDataAccess.java
+++ src/sg/edu/nus/iss/uss/dao/FileDataAccess.java
@@ -1,5 +1,7 @@
 package sg.edu.nus.iss.uss.dao;
 
+import java.util.Objects;
+
 abstract class FileDataAccess {
 	
 	private final String fileName;
@@ -10,6 +12,9 @@ abstract class FileDataAccess {
 	}
 	
 	public FileDataAccess(String fileName, String directory) {
+		Objects.requireNonNull(fileName, "fileName cannot be null");
+		Objects.requireNonNull(directory, "directory cannot be null");
+		
 		this.fileName = fileName;
 		this.directory = directory;
 	}
