50c56ed7b401d7612fc9a0e6d9aa0613778a9259
benjaminng <benjaminng@DESKTOP-DL9RA9V>
2016-03-12 16:23:15 +0800

    add common method for convert string to date

--- src/sg/edu/nus/iss/uss/util/UssCommonUtil.java
+++ src/sg/edu/nus/iss/uss/util/UssCommonUtil.java
@@ -1,15 +1,29 @@
 package sg.edu.nus.iss.uss.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class UssCommonUtil {
 
-	public final static String DATE_FORMAT = "YYYY-MM-dd"; 
+	public final static String DATE_FORMAT = "YYYY-MM-dd";
+	
+	private static final SimpleDateFormat DEFAULT_DATE_FORMATTER = new SimpleDateFormat(DATE_FORMAT);
 	
 	public static String convertDateToString(Date date){
 		//TODO convertDateToString
 		
 		return "";
+	}
+	
+	public static Date convertStringToDate(String date) {
+		try {
+			return DEFAULT_DATE_FORMATTER.parse(date);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
 	
