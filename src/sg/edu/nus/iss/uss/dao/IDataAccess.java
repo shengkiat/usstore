5704cce97e11ad17fbd8e2f2ab0bc6e641f3f020
benjaminng <benjaminng@DESKTOP-DL9RA9V>
2016-03-12 14:01:38 +0800

    add methods

--- src/sg/edu/nus/iss/uss/dao/IDataAccess.java
+++ src/sg/edu/nus/iss/uss/dao/IDataAccess.java
@@ -1,5 +1,11 @@
 package sg.edu.nus.iss.uss.dao;
 
+import java.util.List;
+
 public interface IDataAccess<E> {
+	
+	public List<E> getAll();
+	public void create(E e);
+	public void update(E e);
 
 }
