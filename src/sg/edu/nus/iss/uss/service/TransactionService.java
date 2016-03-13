12cc73bac2589c1ce3c196ecde49d76deef54637
benjaminng <benjaminng@DESKTOP-DL9RA9V>
2016-03-13 19:22:16 +0800

    amend create method until later concrete scenario

--- src/sg/edu/nus/iss/uss/service/TransactionService.java
+++ src/sg/edu/nus/iss/uss/service/TransactionService.java
@@ -17,8 +17,8 @@ public class TransactionService extends UssCommonService {
 		this.transactionDataAccess = transactionDataAccess;
 	}
 	
-	public Transaction createNewTransaction(){
-		return null;
+	public void createTransactions(List<Transaction> transactions){
+		transactionDataAccess.create(transactions);
 	}
 	
 	public List<Transaction> retrieveTransactionListByDate(Date startDate, Date endDate){
