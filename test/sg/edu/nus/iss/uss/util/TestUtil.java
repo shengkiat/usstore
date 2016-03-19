3e14ab00f665e28cac3efc47d2fb9bdc0f0a6e15
richardz316 <jsman316@gmail.com>
2016-03-19 12:53:38 +0800

    -add the Util Method to setup and destory MemberService

--- test/sg/edu/nus/iss/uss/util/TestUtil.java
+++ test/sg/edu/nus/iss/uss/util/TestUtil.java
@@ -1,10 +1,17 @@
 package sg.edu.nus.iss.uss.util;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import sg.edu.nus.iss.uss.dao.filedataaccess.MemberFileDataAccess;
+import sg.edu.nus.iss.uss.service.MemberService;
 
 public class TestUtil {
 	
 	private static final String TEST_DATA_DIR;
+	private static final String TEST_FILE_FOR_MEMBER = "Members.dat";
 	
 	static {
 		StringBuilder builder = new StringBuilder(System.getProperty("user.dir"));
@@ -24,6 +31,68 @@ public class TestUtil {
 	
 	public static String getTestDirectoryForFile() {
 		return TEST_DATA_DIR;
+	}
+	
+	
+	public static MemberService setUpMemberServiceWithThreeMember() throws IOException{
+		String fileName = TEST_DATA_DIR+ File.separator+ TEST_FILE_FOR_MEMBER;
+		initFileWithThreeMembers(fileName);
+		MemberService service = new MemberService(new MemberFileDataAccess(TEST_FILE_FOR_MEMBER, TEST_DATA_DIR));
+		return service;
+	}
+	
+	public static void destoryMemberServiceAndFile(MemberService memberService){
+		
+		memberService = null;
+		destoryFile(TEST_DATA_DIR+ File.separator+ TEST_FILE_FOR_MEMBER);
+	}
+	
+	
+	private static void initFileWithThreeMembers(String fileName) throws IOException{
+		/*
+		 * Create Members.dat file with three members
+		 * 
+		 */
+		
+		String [] lines = new String []{
+			"Yan Martel,F42563743156,150",
+			"Suraj Sharma,X437F356,250",
+			"Ang Lee,R64565FG4,-1"
+		};
+
+		createFileWithLines(fileName, lines);
+		
+	}
+
+	private static void createFileWithLines(String fileFullPath, String[] lines) throws IOException {
+//		File file = new File("C:\\NUS\\project\\usstore\\test\\sg\\edu\\nus\\iss\\uss\\filedataaccess\\Members.dat");
+
+		File file = new File(fileFullPath);
+		
+		// if file doesnt exists, then create it
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+
+		FileWriter fw = new FileWriter(file.getAbsoluteFile());
+		BufferedWriter bw = new BufferedWriter(fw);
+		for(String line : lines){
+			bw.write(line);
+			bw.newLine();
+		}
+		
+		bw.close();
+		
+	}
+	
+	private static void destoryFile(String fileFullPath){
+
+		File file = new File(fileFullPath);
+
+		// if file doesnt exists, then create it
+		if (file.exists()) {
+			file.delete();
+		}
 	}
 
 }
