3cddf8e8a33b956775d7d99a386277bbbb43fb85
ShengKiat <ShengKiat>
2016-03-21 19:27:05 +0800

    Comment out Reporting

--- src/sg/edu/nus/iss/uss/client/Application.java
+++ src/sg/edu/nus/iss/uss/client/Application.java
@@ -18,6 +18,7 @@ import javax.swing.JButton;
 import javax.swing.JList;
 import javax.swing.ListSelectionModel;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 import java.awt.FlowLayout;
 
@@ -39,6 +40,9 @@ import sg.edu.nus.iss.uss.service.impl.ReportingService;
 import sg.edu.nus.iss.uss.service.impl.TransactionService;
 
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -48,9 +52,10 @@ public class Application {
 	//private AuthorisedService authService = new AuthorisedService();
 	private JTextField textFieldBarcode;
 	
+
+	private JPanel rightEnterAmountPanel;
 	
-	
-	private JPanel rightClickPaymentPanel;
+	private JPanel rightFinishPaymentPanel;
 	
 	private JPanel rightMemberPanel ;
 	
@@ -58,6 +63,14 @@ public class Application {
 	private IAuthorisedService authService;
 	private IReportingService reportingService;
 	
+
+	private JTextField txtAmountReceived;
+	private JTextField txtMemberDollarRedem;
+
+	private JLabel lbl1, lbl2, lbl3;
+	private	JLabel lblMemberName, lblMemberLoyaltyPts, lblAmountReceived;
+
+
 	
 	/**
 	 * Launch the application.
@@ -216,21 +229,27 @@ public class Application {
 		frame.getContentPane().add(leftPanel);
 		
 		String[] selections = { "green", "red", "orange", "dark blue" };
-		JList list = new JList(selections);
-		list.setValueIsAdjusting(true);
-		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		
-		JScrollPane scrollPane = new JScrollPane(list);
+		String[] columnNames = {"Product Name","Price"};
+		Object[][] data = { { "Orange", "$4"},
+                { "Apple", "$5"} };
+		JTable tbScanProducts = new JTable(data, columnNames);
+		
+		
+		JList list = new JList(selections);
+		//tbScanProducts.setValueIsAdjusting(true);
+		tbScanProducts.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		
+		JScrollPane scrollPane = new JScrollPane(tbScanProducts);
 		scrollPane.setMaximumSize(new Dimension(500, 200));
 		scrollPane.setMinimumSize (new Dimension(500, 200));
-		Dimension d = list.getPreferredSize();
+		Dimension d = tbScanProducts.getPreferredSize();
 		d.width = 200;
 		leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.PAGE_AXIS));
-		scrollPane.setPreferredSize(new Dimension(500, 500));
-		leftPanel.add(scrollPane);
 		
 		JPanel barcodePanel = new JPanel();
 		leftPanel.add(barcodePanel);
+		barcodePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		JLabel lblBarcode = new JLabel("BarCode:");
 		barcodePanel.add(lblBarcode);
@@ -241,17 +260,138 @@ public class Application {
 		
 		JButton btnAdd = new JButton("Add");
 		barcodePanel.add(btnAdd);
+		scrollPane.setPreferredSize(new Dimension(500, 500));
+		leftPanel.add(scrollPane);
 		
-		 rightClickPaymentPanel = new JPanel();
-		frame.getContentPane().add(rightClickPaymentPanel);
-		rightClickPaymentPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		JPanel subtotalPanel = new JPanel();
+		leftPanel.add(subtotalPanel);
+		
+		JLabel lblTotal = new JLabel("Total:");
+		subtotalPanel.add(lblTotal);
+		
+		JLabel lblsubTotal = new JLabel("500");
+		subtotalPanel.add(lblsubTotal);
+		
+		//TODO
+		rightEnterAmountPanel = new JPanel();
+		//frame.getContentPane().add(rightEnterAmountPanel, BorderLayout.NORTH);
+		GridBagLayout gbl_rightEnterAmountPanel = new GridBagLayout();
+		gbl_rightEnterAmountPanel.columnWidths = new int[]{50, 134, 86, 0};
+		gbl_rightEnterAmountPanel.rowHeights = new int[]{29, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_rightEnterAmountPanel.columnWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_rightEnterAmountPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		rightEnterAmountPanel.setLayout(gbl_rightEnterAmountPanel);
+		
+		lbl1 = new JLabel("Member Name:");
+		GridBagConstraints gbc_lbl1 = new GridBagConstraints();
+		gbc_lbl1.anchor = GridBagConstraints.EAST;
+		gbc_lbl1.insets = new Insets(0, 0, 5, 5);
+		gbc_lbl1.gridx = 1;
+		gbc_lbl1.gridy = 2;
+		rightEnterAmountPanel.add(lbl1, gbc_lbl1);
+		
+		 lblMemberName = new JLabel("New label");
+		GridBagConstraints gbc_lblMemberName = new GridBagConstraints();
+		gbc_lblMemberName.anchor = GridBagConstraints.WEST;
+		gbc_lblMemberName.insets = new Insets(0, 0, 5, 0);
+		gbc_lblMemberName.gridx = 2;
+		gbc_lblMemberName.gridy = 2;
+		rightEnterAmountPanel.add(lblMemberName, gbc_lblMemberName);
+		
+		lbl2 = new JLabel("Member Loyalty Dollars:");
+		GridBagConstraints gbc_lbl2 = new GridBagConstraints();
+		gbc_lbl2.anchor = GridBagConstraints.EAST;
+		gbc_lbl2.insets = new Insets(0, 0, 5, 5);
+		gbc_lbl2.gridx = 1;
+		gbc_lbl2.gridy = 3;
+		rightEnterAmountPanel.add(lbl2, gbc_lbl2);
+		
+		 lblMemberLoyaltyPts = new JLabel("New label");
+		GridBagConstraints gbc_lblMemberLoyaltyPts = new GridBagConstraints();
+		gbc_lblMemberLoyaltyPts.anchor = GridBagConstraints.WEST;
+		gbc_lblMemberLoyaltyPts.insets = new Insets(0, 0, 5, 0);
+		gbc_lblMemberLoyaltyPts.gridx = 2;
+		gbc_lblMemberLoyaltyPts.gridy = 3;
+		rightEnterAmountPanel.add(lblMemberLoyaltyPts, gbc_lblMemberLoyaltyPts);
+		
+		 lbl3 = new JLabel("Member Dollars to Redem:");
+		GridBagConstraints gbc_lbl3 = new GridBagConstraints();
+		gbc_lbl3.anchor = GridBagConstraints.EAST;
+		gbc_lbl3.insets = new Insets(0, 0, 5, 5);
+		gbc_lbl3.gridx = 1;
+		gbc_lbl3.gridy = 4;
+		rightEnterAmountPanel.add(lbl3, gbc_lbl3);
+		
+		txtMemberDollarRedem = new JTextField();
+		GridBagConstraints gbc_txtMemberDollarRedem = new GridBagConstraints();
+		gbc_txtMemberDollarRedem.anchor = GridBagConstraints.WEST;
+		gbc_txtMemberDollarRedem.insets = new Insets(0, 0, 5, 0);
+		gbc_txtMemberDollarRedem.gridx = 2;
+		gbc_txtMemberDollarRedem.gridy = 4;
+		rightEnterAmountPanel.add(txtMemberDollarRedem, gbc_txtMemberDollarRedem);
+		txtMemberDollarRedem.setColumns(10);
+		
+		 lblAmountReceived = new JLabel("Amount Received:");
+		GridBagConstraints gbc_lblAmountReceived = new GridBagConstraints();
+		gbc_lblAmountReceived.anchor = GridBagConstraints.EAST;
+		gbc_lblAmountReceived.insets = new Insets(0, 0, 5, 5);
+		gbc_lblAmountReceived.gridx = 1;
+		gbc_lblAmountReceived.gridy = 6;
+		rightEnterAmountPanel.add(lblAmountReceived, gbc_lblAmountReceived);
+		
+		txtAmountReceived = new JTextField();
+		GridBagConstraints gbc_txtAmountReceived = new GridBagConstraints();
+		gbc_txtAmountReceived.anchor = GridBagConstraints.WEST;
+		gbc_txtAmountReceived.insets = new Insets(0, 0, 5, 0);
+		gbc_txtAmountReceived.gridx = 2;
+		gbc_txtAmountReceived.gridy = 6;
+		rightEnterAmountPanel.add(txtAmountReceived, gbc_txtAmountReceived);
+		txtAmountReceived.setColumns(10);
+		
+		JButton btnMakePayment = new JButton("Make Payment");
+		GridBagConstraints gbc_btnMakePayment = new GridBagConstraints();
+		gbc_btnMakePayment.insets = new Insets(0, 0, 5, 5);
+		gbc_btnMakePayment.gridx = 1;
+		gbc_btnMakePayment.gridy = 8;
+		rightEnterAmountPanel.add(btnMakePayment, gbc_btnMakePayment);
+		
+		JButton btnNewButton = new JButton("Cancel");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton.anchor = GridBagConstraints.NORTHWEST;
+		gbc_btnNewButton.gridx = 2;
+		gbc_btnNewButton.gridy = 8;
+		rightEnterAmountPanel.add(btnNewButton, gbc_btnNewButton);
+		
+		JLabel lbl6 = new JLabel("Change:");
+		GridBagConstraints gbc_lbl6 = new GridBagConstraints();
+		gbc_lbl6.anchor = GridBagConstraints.EAST;
+		gbc_lbl6.insets = new Insets(0, 0, 0, 5);
+		gbc_lbl6.gridx = 1;
+		gbc_lbl6.gridy = 14;
+		rightEnterAmountPanel.add(lbl6, gbc_lbl6);
+		
+		JLabel lblChange = new JLabel("New label");
+		GridBagConstraints gbc_lblChange = new GridBagConstraints();
+		gbc_lblChange.anchor = GridBagConstraints.WEST;
+		gbc_lblChange.gridx = 2;
+		gbc_lblChange.gridy = 14;
+		rightEnterAmountPanel.add(lblChange, gbc_lblChange);
+		
+		 rightFinishPaymentPanel = new JPanel();
+		frame.getContentPane().add(rightFinishPaymentPanel);
+		rightFinishPaymentPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		JButton btnFinishAndPay = new JButton("Finish and Pay");
 		btnFinishAndPay.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// Finish buying items
 				
-				frame.remove(rightClickPaymentPanel);
+				frame.remove(rightFinishPaymentPanel);
 				frame.getContentPane().add(rightMemberPanel);
                 frame.revalidate(); // For Java 1.7 or above.
                 // frame.getContentPane().validate(); // For Java 1.6 or below.
@@ -260,28 +400,42 @@ public class Application {
 			}
 		});
 		btnFinishAndPay.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
-		rightClickPaymentPanel.add(btnFinishAndPay);
+		rightFinishPaymentPanel.add(btnFinishAndPay);
 		
 		rightMemberPanel = new JPanel();
 
 		
 		JButton btnMemberPay = new JButton("Member");
+		btnMemberPay.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				MemberLoginDialog memberLoginDlg = new MemberLoginDialog();
+				
+				memberLoginDlg.setModalityType(ModalityType.TOOLKIT_MODAL);
+				memberLoginDlg.setLocationRelativeTo(null);
+				memberLoginDlg.setVisible(true);
+				
+				if (memberLoginDlg.isMember()){
+					// TODO
+					
+					memberMakePayment();
+				}
+				
+			}
+		});
+		
+		
 		btnMemberPay.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
 		rightMemberPanel.add(btnMemberPay);
-		
-		JButton btnNonMemberPay = new JButton("Non Member");
-		btnNonMemberPay.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
-		rightMemberPanel.add(btnNonMemberPay);
-	
-		JPanel reportingTransactionPanel = new ReportTransactionPanel(frame, reportingService);
+		final JPanel reportingTransactionPanel = new ReportTransactionPanel(frame, reportingService);
 		//reportingTransactionPanel.add(scrollPane);
 		
 		JMenuItem mntmTransactions = new JMenuItem("Transactions");
 		mntmTransactions.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				frame.remove(leftPanel);
-				frame.remove(rightClickPaymentPanel);
-				frame.remove(rightMemberPanel);
+				//frame.remove(leftPanel);
+				//frame.remove(rightClickPaymentPanel);
+				//frame.remove(rightMemberPanel);
 				
 				frame.add(reportingTransactionPanel);
 				
@@ -299,7 +453,55 @@ public class Application {
 		
 	}
 	
+private void memberMakePayment(){
+		
+		lbl1.setVisible(true);
+		lblMemberName.setVisible(true);
+		lbl2.setVisible(true);
+		lbl3.setVisible(true);
+		lblMemberLoyaltyPts.setVisible(true);
+		txtMemberDollarRedem.setVisible(true);
+		
+		
+		
+		
+		
+		
+		frame.remove(rightMemberPanel);
+		frame.getContentPane().add(rightEnterAmountPanel);
+        frame.revalidate(); // For Java 1.7 or above.
+        // frame.getContentPane().validate(); // For Java 1.6 or below.
+        frame.repaint();
+		
+		
+	}
 	
+	private void nonMemberMakePayment(){
+		
+		lbl1.setVisible(false);
+		lblMemberName.setVisible(false);
+		lbl2.setVisible(false);
+		lbl3.setVisible(false);
+		lblMemberLoyaltyPts.setVisible(false);
+		txtMemberDollarRedem.setVisible(false);
+		
+		
+		
+		
+		
+		
+		frame.remove(rightMemberPanel);
+		frame.getContentPane().add(rightEnterAmountPanel);
+        frame.revalidate(); // For Java 1.7 or above.
+        // frame.getContentPane().validate(); // For Java 1.6 or below.
+        frame.repaint();
+		
+		
+	
+		
+		
+		
+	}
 	
 	
 
