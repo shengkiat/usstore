3bbc642783c53faea8c772d9c75dc0449127387b
richardz316 <jsman316@gmail.com>
2016-03-26 16:46:18 +0800

    -remove obsolete class (IStore and UniversitySouvenirStore)

--- src/sg/edu/nus/iss/uss/model/IStore.java
+++
@@ -1,9 +0,0 @@
-package sg.edu.nus.iss.uss.model;
-
-public interface IStore {
-	
-	public abstract void InitStore();//Initialization of Store
-	public abstract String getStoreName();
-	public abstract String getStoreDescription();
-
-}
