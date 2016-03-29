2f69f9f88dfe321edcc7bd7558dd81b9de3b39ce
Goh Chong Jhee <jarongoh@yahoo.com>
2016-03-29 21:12:47 +0800

    29/Mar/16 2112 hrs

--- src/sg/edu/nus/iss/uss/model/Category.java
+++ src/sg/edu/nus/iss/uss/model/Category.java
@@ -4,8 +4,6 @@ public class Category {
 	private String code;
 	private String name;
 	
-	private volatile int hashcode;
-	
 	public Category() {}
 	
 	public Category (String code, String name) {
@@ -28,52 +26,33 @@ public class Category {
 		return name;
 	}
 	public void setName(String name) {
-		if (name.equals(null) ) {
-
-		}
 		//if (name.equals(null) ) {
 			// throws Category description should not be empty
 		//}
-
 		this.name = name;
 	}
 	
 	@Override
 	public String toString() {//CLO,Clothing
-        //System.out.println(code);
+        /*if (this.code == null) {
+        	if (this.name != null) {
+            	this.code = this.code + " , " + this.name;        		
+        	}
+        }*/
+		//System.out.println(code);
 		StringBuilder sb = new StringBuilder();
-		sb.append(this.code);
-		sb.append(",");
-		sb.append(this.name);
+		if (this.code == null) {
+			sb.append(this.code);
+			if (this.name == null) {
+				this.name = "";}
+			else {
+				sb.append(",");
+				sb.append(this.name);}
+			
+		}
 		
-		/*System.out.println("");
-		System.out.println(sb.toString());*/
 		return sb.toString();
 
-	}
-	
-	
-	@Override
-	public int hashCode() {
-		int result = hashcode;
-		if (result ==0 ) {
-		    result = 17;	
-			result = 31 * result + this.code.hashCode();
-			result = 31 * result + this.name.hashCode();
-			hashcode = result;
-		}
-		return result;
-	}
-	
-	@Override
-	public boolean equals(Object o) {
-		if (o == this) return true;
-		if (!(o instanceof Category)) return false;
-		Category c = (Category) o;
-		
-	    return c.code == this.code
-	    && c.name == this.name;
-	    
 	}
 	
 	
