f4d39b651cb8a5e35d5b528ad38cb5a5140227ec
benjaminng <benjaminng@DESKTOP-DL9RA9V>
2016-04-02 21:21:14 +0800

    close when cross

--- src/sg/edu/nus/iss/uss/client/LoginDialog.java
+++ src/sg/edu/nus/iss/uss/client/LoginDialog.java
@@ -84,6 +84,31 @@ public class LoginDialog extends JDialog {
 				dispose();
 			}
 		});
+		
+		this.addWindowListener(new WindowListener() {
+            public void windowActivated(WindowEvent e) {
+            }
+
+            public void windowClosed(WindowEvent e) {
+            }
+
+            public void windowClosing(WindowEvent e) {
+            }
+
+            public void windowDeactivated(WindowEvent e) {
+                System.exit(0);
+            }
+
+            public void windowDeiconified(WindowEvent e) {
+            }
+
+            public void windowIconified(WindowEvent e) {
+            }
+
+            public void windowOpened(WindowEvent e) {
+            }
+        });
+		
 		JPanel bp = new JPanel();
 		bp.add(btnLogin);
 		bp.add(btnCancel);
